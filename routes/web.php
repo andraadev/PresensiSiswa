53164225f04f10bb5fea84e890e9a96b971db6d7
andraadev <107798601+andraadev@users.noreply.github.com>
2025-09-09 16:40:46 +0700

    refactor: remove unused semicolon in route resource

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,9 @@ Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
 
     // Route yang menangani fungsi CRUD
     Route::resource('/data-guru', GuruController::class)->parameters(['data-guru' => 'guru']);
-    Route::resource('/data-siswa', SiswaController::class)->parameters(['data-siswa' => 'siswa']);;
-    Route::resource('/data-kelas', KelasController::class)->parameters(['data-kelas' => 'kelas']);;
-    Route::resource('/data-user', UserController::class)->parameters(['data-user' => 'user']);;
+    Route::resource('/data-siswa', SiswaController::class)->parameters(['data-siswa' => 'siswa']);
+    Route::resource('/data-kelas', KelasController::class)->parameters(['data-kelas' => 'kelas']);
+    Route::resource('/data-user', UserController::class)->parameters(['data-user' => 'user']);
 });
 
 Route::group(['middleware' => 'auth', 'prefix' => 'guru'], function () {
