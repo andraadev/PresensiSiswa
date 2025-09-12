3d4a5479a0220d3c4b7d68e0bb715627fb14aec8
andraadev <107798601+andraadev@users.noreply.github.com>
2025-09-12 21:29:48 +0700

    fix(ui): add fallback in input if validation fail

--- resources/views/admin/data-siswa/create.blade.php
+++ resources/views/admin/data-siswa/create.blade.php
@@ -22,22 +22,28 @@
         </div>
         <div class="mb-3">
             <label class="form-label">Nama Lengkap</label>
-            <input type="nama_lengkap" class="form-control" name="nama_lengkap" value="{{ old('nama_lengkap') }}">
+            <input type="text" class="form-control" name="nama_lengkap" value="{{ old('nama_lengkap') }}">
         </div>
         <div class="mb-3">
             <label class="form-label">Jenis Kelamin</label>
-            <br>
-            <input type="radio" name="jenis_kelamin" id="checkbox_l" value="Laki-laki" class="form-check-input" checked>
-            <label for="checkbox_l">Laki-laki</label> <br>
-
-            <input type="radio" name="jenis_kelamin" id="checkbox_p" value="Perempuan" class="form-check-input">
-            <label for="checkbox_p">Perempuan</label>
+            <div class="form-check">
+                <input type="radio" name="jenis_kelamin" id="checkbox_l" value="Laki-laki" class="form-check-input"
+                    {{ old('jenis_kelamin', 'Laki-laki') == 'Laki-laki' ? 'checked' : '' }} checked>
+                <label for="checkbox_l">Laki-laki</label>
+            </div>
+            <div class="form-check">
+                <input type="radio" name="jenis_kelamin" id="checkbox_p" value="Perempuan" class="form-check-input"
+                    {{ old('jenis_kelamin') == 'Perempuan' ? 'checked' : '' }}>
+                <label for="checkbox_p">Perempuan</label>
+            </div>
         </div>
         <div class="mb-3">
             <label class="form-label">Kelas</label>
             <select name="kelas_id" class="form-select">
                 @forelse ($kelas as $data_kelas)
-                    <option value="{{ $data_kelas->id }}">{{ $data_kelas->nama_kelas }}</option>
+                    <option value="{{ $data_kelas->id }}" {{ old('kelas_id') == $data_kelas->id ? 'selected' : '' }}>
+                        {{ $data_kelas->nama_kelas }}
+                    </option>
                 @empty
                     <option>Data Kelas Kosong</option>
                 @endforelse
