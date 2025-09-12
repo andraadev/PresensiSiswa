a84a4a01f95055d6bc287adf76c2ed04f58b7359
andraadev <107798601+andraadev@users.noreply.github.com>
2025-09-12 21:33:16 +0700

    refactor(update): change input types and add validation fallback

--- resources/views/admin/data-siswa/update.blade.php
+++ resources/views/admin/data-siswa/update.blade.php
@@ -19,40 +19,40 @@
         @method('PUT')
         <div class="mb-3">
             <label class="form-label">NISN</label>
-            <input type="number" class="form-control" name="nisn" value="{{ $siswa->nisn }}">
+            <input type="text" class="form-control" name="nisn" value="{{ old('nisn', $siswa->nisn) }}">
         </div>
         <div class="mb-3">
             <label class="form-label">Nama Lengkap</label>
-            <input type="nama_lengkap" class="form-control" name="nama_lengkap" value="{{ $siswa->nama_lengkap }}">
+            <input type="text" class="form-control" name="nama_lengkap"
+                value="{{ old('nama_lengkap', $siswa->nama_lengkap) }}">
         </div>
         <div class="mb-3">
             <label class="form-label">Jenis Kelamin</label>
-            <br>
-            <input type="radio" name="jenis_kelamin" id="checkbox_l" value="Laki-laki" class="form-check-input"
-                {{ $siswa->jenis_kelamin == 'Laki-laki' ? 'checked' : '' }}>
-            <label for="checkbox_l">Laki-laki</label><br>
-
-            <input type="radio" name="jenis_kelamin" id="checkbox_p" value="Perempuan" class="form-check-input"
-                {{ $siswa->jenis_kelamin == 'Perempuan' ? 'checked' : '' }}>
-            <label for="checkbox_p">Perempuan</label>
+            <div class="form-check">
+                <input type="radio" name="jenis_kelamin" id="checkbox_l" value="Laki-laki" class="form-check-input"
+                    {{ old('jenis_kelamin', $siswa->jenis_kelamin) == 'Laki-laki' ? 'checked' : '' }}>
+                <label class="form-check-label" for="checkbox_l">Laki-laki</label>
+            </div>
+            <div class="form-check">
+                <input type="radio" name="jenis_kelamin" id="checkbox_p" value="Perempuan" class="form-check-input"
+                    {{ old('jenis_kelamin', $siswa->jenis_kelamin) == 'Perempuan' ? 'checked' : '' }}>
+                <label class="form-check-label" for="checkbox_p">Perempuan</label>
+            </div>
         </div>
         <div class="mb-3">
             <label class="form-label">Kelas</label>
             <select name="kelas_id" class="form-select">
                 @foreach ($kelas as $data_kelas)
-                    @if ($data_kelas->id === $siswa->kelas_id)
-                        <option value="{{ $data_kelas->id }}" selected>
-                            {{ $data_kelas->nama_kelas }}</option>
-                    @else
-                        <option value="{{ $data_kelas->id }}">
-                            {{ $data_kelas->nama_kelas }}</option>
-                    @endif
+                    <option value="{{ $data_kelas->id }}"
+                        {{ old('kelas_id', $siswa->kelas_id) == $data_kelas->id ? 'selected' : '' }}>
+                        {{ $data_kelas->nama_kelas }}
+                    </option>
                 @endforeach
             </select>
         </div>
         <div class="mb-3">
             <label class="form-label">Nomor Telepon</label>
-            <input type="tel" class="form-control" name="no_telepon" value="{{ $siswa->no_telepon }}">
+            <input type="tel" class="form-control" name="no_telepon" value="{{ old('no_telepon', $siswa->no_telepon) }}">
         </div>
         <button type="submit" class="btn btn-primary">Simpan</button>
     </form>
