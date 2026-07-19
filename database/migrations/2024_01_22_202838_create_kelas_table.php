9ac3f3cd83a02909d4b8780698ffcd6e7a8974be
andraadev <107798601+andraadev@users.noreply.github.com>
2026-07-19 11:18:05 +0700

    chore(db): add slug_kelas and qr_code fields to kelas table

--- database/migrations/2024_01_22_202838_create_kelas_table.php
+++ database/migrations/2024_01_22_202838_create_kelas_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('kelas', function (Blueprint $table) {
             $table->id();
             $table->string('nama_kelas', 20);
+            $table->string('slug_kelas', 20);
+            $table->string('qr_code', 100);
             $table->foreignId('guru_id')->references('id')->on('guru');
             $table->timestamps();
         });
