fc71c6c288450d589f2606e72cf206f17f808150
andraadev <107798601+andraadev@users.noreply.github.com>
2026-08-02 15:28:24 +0700

    refactor(absensi): standardize admin attendance route name

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::group(['middleware' => ['auth', 'checkrole:Admin'], 'prefix' => 'admin'], function () {
     Route::get('/beranda', [DashboardController::class, 'beranda_admin'])->name('admin.beranda');
-    Route::get('/data-absensi', [DashboardController::class, 'data_absensi'])->name('data_absensi');
+    Route::get('/data-absensi', [DashboardController::class, 'data_absensi'])->name('admin.data_absensi');
     Route::get('/rekapitulasi-absensi', [DashboardController::class, 'rekapitulasi_absensi'])->name('admin.rekapitulasi');
     Route::get('/download/{kelas}', [KelasController::class, 'download_qr'])->name('admin.data_kelas.download_qr');
 
