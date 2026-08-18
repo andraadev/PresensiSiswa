d08f1daf28ae11c87972c1b65dfcf0f87ec76287
andraadev <107798601+andraadev@users.noreply.github.com>
2026-08-18 21:53:54 +0700

    fix(modal): update logout confirmation message and button colors

--- public/assets/js/logout-modal.js
+++ public/assets/js/logout-modal.js
@@ -1,12 +1,13 @@
 function modal_logout() {
     Swal.fire({
-        title: 'Peringatan',
-        text: 'Apakah kamu yakin ingin logout?',
-        icon: 'warning',
+        title: 'Konfirmasi',
+        text: 'Apakah kamu yakin ingin keluar dari aplikasi?',
         confirmButtonText: 'Iya',
         cancelButtonText: 'Tidak',
-        confirmButtonColor: '#d33',
-        cancelButtonColor: '#3085d6',
+        // bootstrap 5 danger color hex
+        confirmButtonColor: '#dc3545',
+        // bootstrap 5 primary color hex
+        cancelButtonColor: '#0d6efd',
         showCancelButton: true
     }).then((result) => {
         if (result.isConfirmed) {
